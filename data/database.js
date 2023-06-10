f27d7ef5f8d5985e0400b8943e8197a64715d4a1
Sahil Tambe <sahiltambe81922@gmail.com>
2023-06-10 16:49:52 +0530

    some chnages

--- data/database.js
+++ data/database.js
@@ -1,21 +1,24 @@
 const mongodb = require('mongodb');
+const dotenv=require("dotenv").config()
 
 const MongoClient = mongodb.MongoClient;
 
-let mongodbUrl = 'mongodb://127.0.0.1:27017';
+// let mongodbUrl = 'mongodb://127.0.0.1:27017';
 
 
-if (process.env.MONGODB_URL) {
-  mongodbUrl = process.env.MONGODB_URL;
-}
+// if (process.env.MONGODB_URL) {
+ let mongodbUrl = process.env.MONGODB_URL;
+// }
 
+
+console.log(mongodbUrl)
 let database;
 
 async function connectToDatabase() {
   const client = await MongoClient.connect(
     mongodbUrl
-  );
-  database = client.db('online-store');
+    );
+    database = client.db('online-store');
 }
 
 function getDb() {
